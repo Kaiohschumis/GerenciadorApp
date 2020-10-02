763474c82e40e85162be1253bf27f62d659ca23a
Kaiohschumis <38558407+Kaiohschumis@users.noreply.github.com>
2020-10-02 18:29:54 -0300

    correção de anotação

--- src/main/java/com/eventosapp/models/Pontuacao.java
+++ src/main/java/com/eventosapp/models/Pontuacao.java
@@ -7,6 +7,8 @@ import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
 import javax.persistence.Id;
+import javax.validation.constraints.NotNull;
+
 import org.hibernate.validator.constraints.NotEmpty;
 
 
@@ -22,7 +24,7 @@ public class Pontuacao implements Serializable{
 	@NotEmpty
 	private String jogo;
 	
-	@NotEmpty
+	@NotNull
 	private int placar;
 
 	@NotEmpty
